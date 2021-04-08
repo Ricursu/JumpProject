5a334a92316c09c3e87074a5a7655f52abfded89
xia-mengxi <1538026111@qq.com>
2021-04-09 00:01:48 +0800

    commit

---
+++ Assets/Scripts/CrcExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CrcExtensions : 
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 校验和使用的一组常用操作方法
+    /// </summary>
+    public static ushort ByteSwap(this ushort value)
+    {
+        return (ushort)(((value & 0xff00) >> 8) | (value & 0x00ff) << 8);
+    }
+
+    public static ushort ByteSwapCompliment(this ushort value)
+    {
+        return ByteSwap((ushort)(~value));
+    }
+}
---
+++ Assets/Scripts/UnknowAlgorithmException.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization;
+
+public  class UnknowAlgorithmException : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Crc16Algorithm Algorithm { get; set; }
+
+    public UnknownAlgorithmException()
+    {
+    }
+
+    public UnknownAlgorithmException(Crc16Algorithm algorithm)
+    {
+        this.Algorithm = algorithm;
+    }
+
+    public UnknownAlgorithmException(string message) : base(message)
+    {
+    }
+
+    public UnknownAlgorithmException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected UnknownAlgorithmException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
